70e497ca605724d887c34cdfb7b47edc711b2b63
Your Name <you@example.com>
2024-11-03 15:23:16 +0700

    🐛

--- config/mail.php
+++ config/mail.php
@@ -36,6 +36,12 @@ return [
     */
 
     'mailers' => [
+        'mailgun' => [
+            'transport' => 'mailgun',
+            // 'client' => [
+            //     'timeout' => 5,
+            // ],
+        ],
 
         'smtp' => [
             'transport' => 'smtp',
