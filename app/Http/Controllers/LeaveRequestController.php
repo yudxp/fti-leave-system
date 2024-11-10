fdec8dd0ea58a96d75d24091df6781ae58da0a5e
Your Name <you@example.com>
2024-11-10 18:18:54 +0700

    🐛

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 use App\Models\LeaveRequest;
 use Barryvdh\DomPDF\Facade\Pdf;
-
+use Filament\Notifications\Notification;
 class LeaveRequestController extends Controller
 {
     //
     public function approve(LeaveRequest $record)
     {
         $record->update(['status' => 'approved']);
+
+        $recipient = Employee::find($record->employee_id)->user;
+ 
+        Notification::make()
+            ->title('Leave request approved')
+            ->sendToDatabase($recipient);
+
         return redirect()->back()->with('success', 'Leave request approved successfully');
     }
 
